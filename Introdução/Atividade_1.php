f6f8d083f816c74bc631dc69843e427e3bba99ca
Kaio Martinez Jorge <kaiomartinez831@gmail.com>
2026-04-23 10:55:55 -0300

    Introdução ao PHP como instrumento de DOM

---
+++ Introdução/Atividade_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Empresa Tãnãnã</h1>
+    <!-- Utilização de php para usar tags de HTML -->
+    <?php 
+    echo'<h2 style="text-align: center;"> Seja bem vindo(a) </h2>';
+    echo '<ul>
+        <li> Produto 1 </li>
+        <li> Produto 2 </li>
+        <li> Produto 3 </li>
+    </ul>';
+    ?>
+</body>
+</html>
---
+++ Introdução/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Estou aprendendo PHP</h1>
+    <?php
+
+    echo 'Este é o PHP exibindo um texto';
+    echo '<h2>Aqui o PHP está exibindo um H2
+    em HTML </h2>'
+    ?>
+    
+</body>
+</html>
