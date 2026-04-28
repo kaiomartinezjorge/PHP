2253d2f04cc8aa2c7217ea7fbf4f0378260739cd
Kaio Martinez Jorge <kaiomartinez831@gmail.com>
2026-04-28 10:02:30 -0300

    Exemplos

---
+++ Aula_28-04/Exemplos/EX-3.php
@@ -0,0 +1,9 @@
+<?php
+$funcionarios = 40;
+
+if ($funcionarios >= 50) {
+	echo "Empresa de medio porte";
+} else {
+	echo "Empresa de pequeno porte";
+}
+?>
---
+++ Aula_28-04/Exemplos/EX-4.php
@@ -0,0 +1,11 @@
+<?php
+$funcionarios = 120;
+
+if ($funcionarios < 50) {
+	echo "Pequeno porte";
+} elseif ($funcionarios < 100) {
+	echo "Medio porte";
+} else {
+	echo "Grande porte";
+}
+?>
